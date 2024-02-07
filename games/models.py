875af63fc5a029c155d0027bfe8cdbdd29037320
Meelad <mazizollah@soldevelo.com>
2024-02-07 13:27:03 +0100

    made the model, installed the app and migrated

--- games/models.py
+++ games/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+class Shooter(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class ShooterGame(models.Model):
+    title = models.CharField(max_length=100)
+    genre = models.ForeignKey(Shooter, on_delete=models.CASCADE)
+    release = models.DateTimeField('Release Date')
+    publisher = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    def __str__(self):
+        return self.title and self.release
