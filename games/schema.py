699d5605f9321da358a43a4b2c1dbe7b10a57e12
Meelad <mazizollah@soldevelo.com>
2024-02-08 10:58:35 +0100

    overwritten query class in previous commit, fixed it

--- games/schema.py
+++ games/schema.py
@@ -1,6 +1,5 @@
 import graphene
 from graphene_django import DjangoObjectType
-
 from .models import Shooter, ShooterGame, RPGGame, RPG, SportsGame, Sports
 
 
@@ -9,14 +8,44 @@ class ShooterType(DjangoObjectType):
         model = Shooter
         fields = ('id', 'name', 'shootergame')
 
+
 class ShooterGameType(DjangoObjectType):
     class Meta:
         model = ShooterGame
         fields = ('id', 'title', 'genre', 'rating', 'release', 'publisher', 'description')
 
+
+class RPGType(DjangoObjectType):
+    class Meta:
+        model = RPG
+        fields = ('id', 'name', 'rpggame')
+
+
+class RPGGameType(DjangoObjectType):
+    class Meta:
+        model = RPGGame
+        fields = ('id', 'title', 'genre', 'rating', 'release', 'publisher', 'description')
+
+
+class SportsType(DjangoObjectType):
+    class Meta:
+        model = Sports
+        fields = ('id', 'name', 'sportsgame')
+
+
+class SportsGameType(DjangoObjectType):
+    class Meta:
+        model = SportsGame
+        fields = ('id', 'title', 'genre', 'rating', 'release', 'publisher', 'description')
+
+
 class Query(graphene.ObjectType):
     all_shootergame = graphene.List(ShooterGameType)
     shooter_by_name = graphene.Field(ShooterType, name=graphene.String(required=True))
+    all_rpggame = graphene.List(RPGGameType)
+    rpg_by_name = graphene.Field(RPGType, name=graphene.String(required=True))
+    all_sportsgame = graphene.List(SportsGameType)
+    sports_by_name = graphene.Field(SportsType, name=graphene.String(required=True))
 
     def resolve_all_shootergame(root, info):
         return ShooterGame.objects.select_related('genre').all()
@@ -27,20 +56,6 @@ class Query(graphene.ObjectType):
         except Shooter.DoesNotExist:
             return None
 
-class RPGType(DjangoObjectType):
-    class Meta:
-        model = RPG
-        fields = ('id', 'name', 'rpggame')
-
-class RPGGameType(DjangoObjectType):
-    class Meta:
-        model = RPGGame
-        fields = ('id', 'title', 'genre', 'rating', 'release', 'publisher', 'description')
-
-class Query(graphene.ObjectType):
-    all_rpggame = graphene.List(RPGGameType)
-    rpg_by_name = graphene.Field(RPGType, name=graphene.String(required=True))
-
     def resolve_all_rpggame(root, info):
         return RPGGame.objects.select_related('genre').all()
 
@@ -49,21 +64,6 @@ class Query(graphene.ObjectType):
             return RPG.objects.get(name=name)
         except RPG.DoesNotExist:
             return None
-
-
-class SportsType(DjangoObjectType):
-    class Meta:
-        model = Sports
-        fields = ('id', 'name', 'sportsgame')
-
-class SportsGameType(DjangoObjectType):
-    class Meta:
-        model = SportsGame
-        fields = ('id', 'title', 'genre', 'rating', 'release', 'publisher', 'description')
-
-class Query(graphene.ObjectType):
-    all_sportsgame = graphene.List(RPGGameType)
-    sports_by_name = graphene.Field(RPGType, name=graphene.String(required=True))
 
     def resolve_all_sportsgame(root, info):
         return SportsGame.objects.select_related('genre').all()
