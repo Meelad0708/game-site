d0c5c302bd9cfdb1d504d50211ca01c62c89cc1a
Meelad <mazizollah@soldevelo.com>
2024-02-11 16:19:55 +0100

    added rest to project

--- games/urls.py
+++ games/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework import routers
+from .views import ShooterViewSet, ShooterGameViewSet, RPGViewSet, RPGGameViewSet, SportsViewSet, SportsGameViewSet
+
+router = routers.DefaultRouter()
+router.register(r'shooters', ShooterViewSet)
+router.register(r'shootergames', ShooterGameViewSet)
+router.register(r'rpg', RPGViewSet)
+router.register(r'rpggames', RPGGameViewSet)
+router.register(r'sports', SportsViewSet)
+router.register(r'sportsgames', SportsGameViewSet)
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include(router.urls)),
 ]
--- games/views.py
+++ games/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
-from .models import Shooter, ShooterGame, RPG, RPGGame, Sports
+from rest_framework import viewsets
+from .models import Shooter, ShooterGame, RPG, RPGGame, Sports, SportsGame
+from .game_serializers import ShooterGameSerializer, ShooterSerializer, RPGGameSerializer, RPGSerializer, \
+    SportsSerializer, SportsGameSerializer
+
 
 # Create your views here.
-def index(request):
-    return HttpResponse('hello world')
+class ShooterViewSet(viewsets.ModelViewSet):
+    queryset = Shooter.objects.all()
+    serializer_class = ShooterSerializer
+
+
+class ShooterGameViewSet(viewsets.ModelViewSet):
+    queryset = ShooterGame.objects.all()
+    serializer_class = ShooterGameSerializer
+
+
+class RPGViewSet(viewsets.ModelViewSet):
+    queryset = RPG.objects.all()
+    serializer_class = RPGSerializer
+
+
+class RPGGameViewSet(viewsets.ModelViewSet):
+    queryset = RPGGame.objects.all()
+    serializer_class = RPGGameSerializer
+
+
+class SportsViewSet(viewsets.ModelViewSet):
+    queryset = Sports.objects.all()
+    serializer_class = SportsSerializer
+
+
+class SportsGameViewSet(viewsets.ModelViewSet):
+    queryset = SportsGame.objects.all()
+    serializer_class = SportsGameSerializer
